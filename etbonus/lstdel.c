4d38cedb535de70495db6cf30ab73c4817d0e5ac
paul Haratyk <paulo@iPaul-VIII.local>
2018-11-11 22:09:47 +0100

    a compiler

---
+++ etbonus/lstdel.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list *next;
+	t_list *actu;
+	
+	actu = *alst;
+	while (actu)
+	{
+		next = alst->next;
+		del(actu->content,actu->content_size);
+		free(actu);
+		actu = next;
+	}
+	*alst = NULL;
+}
---
+++ etbonus/lstmap.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*out;
+
+	out = lst;
+	if (lst)
+	{
+		out = f(lst);
+		out->next = ft_lstmap(lst->next, f);
+		return (out);
+	}
+	return (NULL);
+}
